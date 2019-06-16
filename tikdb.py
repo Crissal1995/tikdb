f18b20103812b92111aa6fdd0e7b5e289cb7ab1f
Crissal1995 <blackpowah95@gmail.com>
2019-06-16 09:37:24 +0200

    Update tikdb.py

--- tikdb.py
+++ tikdb.py
@@ -1,29 +1,27 @@
-import os, glob, shutil
-import tarfile, zipfile
+import os
+import glob
+import shutil
+import re
+import tarfile
+import zipfile
 import requests
-
-# debug
-from tkinter import *
-app = Tk()
-app.mainloop()
 
 # setup struct for tids
 base_titleid = '0005000'
-titleids = {'game': base_titleid + '0',
-            'demo': base_titleid + '2',
-            'dlc': base_titleid + 'C',
-            'update': base_titleid + 'E' }
+titleids = {'game': base_titleid + '0', 'demo': base_titleid + '2',
+            'dlc': base_titleid + 'C', 'update': base_titleid + 'E'}
 
 # setup temp folder
 root = os.getcwd()
-tmpfold = "tikdb_tmpfold"
+tmpfold = 'tikdb_tmpfold'
 try: 
     os.mkdir(tmpfold)
 except FileExistsError:
-    print("[WARNING] Temp folder was already created")
-    shutil.rmtree(tmpfold,True)
+    print('[WARNING] Temp folder was already created')
+    shutil.rmtree(tmpfold, ignore_errors=True)
     os.mkdir(tmpfold)
-os.chdir(tmpfold) # cwd: tmpfold
+os.chdir(tmpfold)  # cwd: tmpfold
+
 
 # download titledb
 def parse_titledb():
@@ -31,21 +29,25 @@ def parse_titledb():
     r = requests.get(url)
     if r.status_code != 200:
         raise requests.HTTPError('Cannot connect to title database!')
-    text = r.text
-    _titles = []; _names = []; _regions = []
+    text = r.text  # get text utf8
+    _titles = []  # make arrays
+    _names = []  # for parsing
+    _regions = []  # data from text
     # compiling regex for speed
     title_pattern = re.compile(base_titleid + r'\w-\w{8}', re.I)
     name_pattern = re.compile(r'(?<=<td>)[^\n]*')
     fix_name_pattern = re.compile(r'[\\/:"*?<>|]+')
     reg_pattern = re.compile(r'(?<=<td>)(EUR)|(JAP)|(JPN)|(USA)|(ALL)', re.I)
     # first search
-    result = title_pattern.search(text,0)
+    result = title_pattern.search(text, 0)
     while result is not None:
         start_title, end_title = result.span()
         # search for the next title (end limiter)
-        next_title_result = title_pattern.search(text,end_title)
-        if next_title_result is None: next_title_start = len(text)
-        else: next_title_start, _ = next_title_result.span()
+        next_title_result = title_pattern.search(text, end_title)
+        if next_title_result is None:
+            next_title_start = len(text)
+        else:
+            next_title_start, _ = next_title_result.span()
         # parse title
         title = text[start_title:end_title].upper().replace('-','')
         # check if title is valid, otherwise skip it
@@ -53,23 +55,24 @@ def parse_titledb():
             result = next_title_result
             continue
         # parse name
-        result = name_pattern.search(text,end_title,next_title_start)
+        result = name_pattern.search(text, end_title, next_title_start)
         if result is None: 
             result = next_title_result
             continue
         start_name, end_name = result.span()
         name = text[start_name:end_name]
         # parse region
-        result = reg_pattern.search(text,end_name,next_title_start)
+        result = reg_pattern.search(text, end_name, next_title_start)
         if result is None: 
             result = next_title_result
             continue
         start_reg, end_reg = result.span()
         region = text[start_reg:end_reg].upper()
         # fix name for win folders
-        name = fix_name_pattern.sub('',name)
+        name = fix_name_pattern.sub('', name)
         # fix JAP region
-        if region == 'JAP': region = 'JPN'
+        if region == 'JAP':
+            region = 'JPN'
         # save title parsed
         _titles.append(title)
         _names.append(name)
@@ -78,22 +81,27 @@ def parse_titledb():
         result = next_title_result
     return _titles, _names, _regions
 
+
 # fix names (different names for same game+upd+dlc)
 def fix_names(_titles, _names, _regions):
     for index, _ in enumerate(_names):
         titleid = _titles[index]
         type_titleid = titleid[:8]
         gameid = titleid[8:]
-        if type_titleid in [titleids['dlc'],titleids['update']]:
-            try: game_idx = _titles.index(titleids['game'] + gameid)
-            except ValueError: continue # no game linked to upd/dlc
+        if type_titleid in (titleids['dlc'], titleids['update']):
+            try:
+                game_idx = _titles.index(titleids['game'] + gameid)
+            except ValueError:
+                continue  # no game linked to upd/dlc
             _names[index] = _names[game_idx]
             _regions[index] = _regions[game_idx]
+
 
 print('Parsing titledb...')
 titles, names, regions = parse_titledb()
 fix_names(titles, names, regions)
 print('Titledb parsed')
+
 
 # download ticket db
 def download_tickets():
@@ -113,8 +121,8 @@ def download_tickets():
         os.remove(vaultdb)
         # search for tickets
         for dirpath, _, files in os.walk(os.getcwd()):
-            tiks = glob.glob(os.path.join(dirpath,'*.tik'))
-            if tiks: # len(tiks) > 0
+            tiks = glob.glob(os.path.join(dirpath, '*.tik'))
+            if tiks:  # len(tiks) > 0
                 os.chdir(dirpath)
                 return
         # if here, no tik found
@@ -126,47 +134,58 @@ def download_tickets():
         print('Cannot download tickets!')
         quit(1)
 
+
 print('Downloading tickets...')
 download_tickets()
 print('Tickets downloaded')
 
 # make region folders
-regs = ['EUR','USA','JPN']
+regs = ['EUR', 'USA', 'JPN']
 for reg in regs:
-    try: os.mkdir(reg)
+    try:
+        os.mkdir(reg)
     except FileExistsError:
         print('[WARNING] {} folder was already created'.format(reg))
         shutil.rmtree(reg, ignore_errors=True)
         os.mkdir(reg)
 
+
 # find matches
 def match_tiks():
     def doit(region_game: str, game_name: str, tik_upper: str, tik_original: str):
-        os.chdir(region_game) # (tiks)/(REG)/
-        try: os.mkdir(game_name) # same game can have multiple folders (dlcs and updates)
-        except FileExistsError: pass
-        os.chdir(game_name) # (tiks)/REG/game_name/
-        try: os.mkdir(tik_upper)
-        except FileExistsError: return
-        os.chdir('../..') # (tiks)/
+        os.chdir(region_game)  # (tiks)/REG/
+        try:
+            os.mkdir(game_name)  # same game can have multiple folders (dlcs and updates)
+        except FileExistsError:
+            pass
+        os.chdir(game_name)  # (tiks)/REG/game_name/
+        try:
+            os.mkdir(tik_upper)
+        except FileExistsError:  # same tik got discarded
+            return
+        os.chdir('../..')  # (tiks)/
         title_path = os.path.normpath(region_game + '/' + game_name + '/' + tik_upper)
-        shutil.copyfile(tik_original, os.path.join(title_path, 'title.tik') )
+        shutil.copyfile(tik_original, os.path.join(title_path, 'title.tik'))
 
     for tik in glob.glob('*.tik'):
-        tik_name = tik.replace('.tik','').upper()
-        try: index = titles.index(tik_name)
-        except ValueError: continue
+        tik_name = tik.replace('.tik', '').upper()
+        try:
+            index = titles.index(tik_name)
+        except ValueError:
+            continue
         name = names[index]
         region = regions[index]
-        if region == 'ALL': # 'ALL' region
+        if region == 'ALL':  # 'ALL' region
             for region in regs:
                 doit(region, name, tik_name, tik)
-        else: # single region
+        else:  # single region
             doit(region, name, tik_name, tik)
+
 
 print('Matching titles...')
 match_tiks()
 print('Titles matched')
+
 
 def zip_all(_zipname: str):
     def zipdir(path, ziph):
@@ -180,8 +199,9 @@ def zip_all(_zipname: str):
         comp = zipfile.ZIP_STORED
     zipf = zipfile.ZipFile(_zipname, 'w', compression=comp)
     for _reg in regs:
-        zipdir(_reg,zipf)
+        zipdir(_reg, zipf)
     zipf.close()
+
 
 print('Creating zip file...')
 zipname = 'tikdb.zip'
